292894aa25584ba52f3fdf9e1486d5e767d9278d
Lotte Koblens <lotte.koblens@hva.nl>
2021-12-07 16:12:49 +0100

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        {/* give nav a class of navbar */}
+        <h1>Waar ga je naartoe?</h1>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
