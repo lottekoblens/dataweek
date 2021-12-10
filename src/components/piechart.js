0ce9a9209b57560abceb4aa41057baf7cc13ae43
Lotte Koblens <lotte.koblens@hva.nl>
2021-12-10 10:16:56 +0100

    deleted background of charts

--- src/components/piechart.js
+++ src/components/piechart.js
@@ -7,7 +7,7 @@ const Piechart = () => {
                 gebruikt door mensen die naar hun werk gaan. En daarna zijn het de studenten die
                 op weg zijn naar school of naar huis.
             </p>
-                <img id="dataset" src="../3d-pie.png"></img>
+                <img id="dataset" src="../pie-3d.png"></img>
             </div>
             <div>
                 <h2>Conclusie</h2>
@@ -16,7 +16,7 @@ const Piechart = () => {
                 de cijfers van het CBS. Het CBS laat namelijk zien dat de meeste reizigers mensen zijn die
                 onderweg zijn van of naar hun werk.
             </p>
-            <img src="./vergelijking.png"></img>
+            <img src="./vergelijking-conclusie.png"></img>
             </div>
         </div>
     );
